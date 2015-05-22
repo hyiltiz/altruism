db57ed4c1069e9489e0dc14d48eb8801fb64cd66
whxpsy <whxpsy@pku.edu.cn>
2015-05-22 21:16:05 +0800

    清除不必要的文件

--- Tl_eval/Psyfeng/TL_Eval/drawStim2.m
+++
@@ -1,6 +0,0 @@
-function drawStim2(wptr,r1,r2,color,cx,cy)
-%drawStim2.m
-%2011-4-4
-%fengchengzhi@suda.edu.cn
-Screen('FrameOval',wptr,color,CenterRectOnPoint([0 0 r1*2 r1*2],cx,cy),r1-r2);
-end
--- tiaoshi.m
+++
@@ -1,8 +0,0 @@
-t1 = tic;
-
-while toc(t1) > 4
-   % disp('f');
-    t2 = tic;
-    toc(t2);
-   
-end
