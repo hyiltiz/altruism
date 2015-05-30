bc05d457d29cbafd862f76b19163c146a54d31f0
Hormetjan Yiltiz <hyiltiz@gmail.com>
2015-05-30 20:30:20 +0800

    INI

--- initializeSeq.m
+++ initializeSeq.m
@@ -6,41 +6,41 @@ block_num = 27; % seq(:,1)
 
 % altruism : zhidaoyu + kongping + 4 altruism + kongping + 4 shocks +
 % kongping
-for i = 1:size(seq(:,1))
-   
-   if seq(i, 1) == 1 % block
-       if seq(i, 2) == 1 % altruism
-           zhidaoyu();
-           zhu_shi_dian();
-           %four_altruism_trials()
-           zhu_shi_dian();
-           %four_shocks();
-           if seq(i, 5) == 1
-               yi_fen_shock()
-           elseif seq(i, 5) == 4
-               si_fen_shock()
-           elseif seq(i, 5) == 6
-               liu_fen_shock()
-           end
-           zhu_shi_dian();
-       end
-       if seq(i, 2) == 2 % zili
-           zhidaoyu();
-           zhu_shi_dian();
-           four_zili_trials()
-           zhu_shi_dian();
-           four_shocks();
-           zhu_shi_dian();
-       end
-       if seq(i, 2) == 3 % ziti
-           zhidaoyu();
-           zhu_shi_dian();
-           four_ziti_trials()
-           zhu_shi_dian();
-           four_shocks();
-           zhu_shi_dian();
-       end
-   end
-       
+item_con = item_liangbiao;
+
+counterOption = 0;
+counterShock = 0;
+
+for i = 1:numel(unique(seq(:,1))) % 27
+    for ii = 1: numel([seq(1,:)==i]) % 4
+    zhidaoyu(seq(i,2));
+    zhu_shi_dian();
+    option1 = item_con.al_item(seq(i*27 + counterOption, 3));
+    option2 = item_con.al_item(seq(i, 4));
+    
+    switch seq(i, 2)
+        case 1% altruism
+            al_trial(option1, option2)
+        case 2
+            four_zili_trials()
+        case 3
+            four_ziti_trials()
+        otherwise
+            error('Bad Condition!');
+    end
+    counterOption = counterOption + 1;
+    end
+counterOption = 0;
+
+for iii=1:4 % this is shock
+    
+    qiangdu_shock(seq(i, 5));
+    
+    counterShock = counterShock+ 1;
 end
+counterShock = 0;
+zhu_shi_dian();
+
+end
+
 end
