7109c83b8515198cf7c7c59408cf265669914b9f
Hormetjan Yiltiz <hyiltiz@gmail.com>
2015-06-02 17:27:55 +0800

    添加 昨晚的 防止 【预测利他部分】 连续几个shock都是同一个强度的内容

--- genseq_choice.m
+++ genseq_choice.m
@@ -9,7 +9,7 @@ function seq_shock = genseq_choice()
 goodFlag = 0;
 while ~goodFlag
     A1 = Shuffle([1:18 1:18]); %18 altruism items
-    A2 = Shuffle([19:36 19:36]); %18 liji items  
+    A2 = Shuffle([19:36 19:36]); %18 liji items
     if any(diff([A1;A2],1,2) == 0)
         % do nothing
     else
@@ -18,8 +18,21 @@ while ~goodFlag
     end
 end
 op_items = [A1' A2'];
-shock_num = genTrials(1,1,[12 3]);
-shockNum = shock_num(:,3);
+
+
+
+goodFlag = 0;
+while ~goodFlag
+    shock_num = genTrials(1,1,[12 3]);
+    shockNum = shock_num(:,3);
+    CC = countConsecutiveMode(shockNum);
+    if any(CC(:,1)>2) % >2 meaning no consecutive 3 same shocks
+        % do nothing
+    else
+        % we are good
+        goodFlag = 1;
+    end
+end
 
 seq_shock = [op_items shockNum];
 
