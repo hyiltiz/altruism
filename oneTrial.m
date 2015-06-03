ac45a3be59536bd9cc4fe71eb27f084238ac18b9
whxpsy <whxpsy@pku.edu.cn>
2015-06-03 20:28:08 +0800

    添加 trialType变量，在put_option据此变量记录

--- oneTrial.m
+++ oneTrial.m
@@ -3,20 +3,23 @@ function oneTrial(wptr, trial_type, ziti_size, xcenter, ycenter, wrect, options,
 
 switch trial_type
     case 1 % altruism trial
-        altruism_trial(wptr, ziti_size, xcenter, ycenter, wrect, options, msg1);
+        trialType = 1;
+        altruism_trial(wptr, ziti_size, xcenter, ycenter, wrect, options, msg1, trialType);
 
     case 2 % self-interest
-        zili_trial(wptr, ziti_size, xcenter, ycenter, wrect, options, msg1);
+        trialType = 2;
+        zili_trial(wptr, ziti_size, xcenter, ycenter, wrect, options, msg1, trialType);
 
     case 3 % xieti
-        ziti_trial(wptr, ziti_size, xcenter, ycenter, wrect, options, msg1);
+        trialType = 3;
+        ziti_trial(wptr, ziti_size, xcenter, ycenter, wrect, options, msg1, trialType);
 
     otherwise
         error('Unknown trial type!');
 end
 end
 
-function  altruism_trial(wptr, ziti_size, xcenter, ycenter, wrect, options, msg1)
+function  altruism_trial(wptr, ziti_size, xcenter, ycenter, wrect, options, msg1, trialType)
 
 left_center = xcenter - 12*ziti_size;
 right_center = xcenter + 1*ziti_size;
@@ -24,13 +27,13 @@ up_center = ycenter - 2*ziti_size;
 down_center = ycenter + 1*ziti_size;
 
 % option_al(wptr, ziti_size,  xcenter, left_center, right_center, up_center, down_center, options, msg1);
-put_option(wptr, NaN, ziti_size,  xcenter, left_center, right_center, up_center, down_center, options, msg1); % NaN means default
+put_option(wptr, NaN, ziti_size,  xcenter, left_center, right_center, up_center, down_center, options, msg1, trialType); % NaN means default
 recordEvents(NaN, 12, NaN, NaN);
 
 jitter_isi(wptr, wrect,myRand(1,3));
 end
 
-function  zili_trial(wptr, ziti_size, xcenter, ycenter, wrect, options, msg1)
+function  zili_trial(wptr, ziti_size, xcenter, ycenter, wrect, options, msg1, trialType)
 
 left_center = xcenter - 12*ziti_size;
 right_center = xcenter + 1*ziti_size;
@@ -38,14 +41,14 @@ up_center = ycenter - 2*ziti_size;
 down_center = ycenter + 1*ziti_size;
 
 % option_zili(wptr, ziti_size, xcenter, left_center, right_center, up_center, down_center, options, msg1);
-put_option(wptr, NaN, ziti_size,  xcenter, left_center, right_center, up_center, down_center, options, msg1); % NaN means default
+put_option(wptr, NaN, ziti_size,  xcenter, left_center, right_center, up_center, down_center, options, msg1, trialType); % NaN means default
 recordEvents(NaN, 22, NaN, NaN);
 
 jitter_isi(wptr, wrect,myRand(1,3));
 end
 
 
-function ziti_trial(wptr, ziti_size, xcenter, ycenter, wrect, options, msg1)
+function ziti_trial(wptr, ziti_size, xcenter, ycenter, wrect, options, msg1, trialType)
 
 left_center = xcenter - 12*ziti_size;
 right_center = xcenter + 1*ziti_size;
@@ -58,7 +61,7 @@ if rand > 0.5
 else
     which_font = 0;
 end
-put_option(wptr, which_font, ziti_size,  xcenter, left_center, right_center, up_center, down_center, options, msg1);
+put_option(wptr, which_font, ziti_size,  xcenter, left_center, right_center, up_center, down_center, options, msg1, trialType);
 recordEvents(NaN, 32, NaN, NaN);
 
 jitter_isi(wptr, wrect,myRand(1,3));
--- put_option.m
+++ put_option.m
@@ -1,4 +1,4 @@
-function put_option(wptr, which_font, ziti_size, xcenter, left_center, right_center, up_center, down_center,options, msg1)
+function put_option(wptr, which_font, ziti_size, xcenter, left_center, right_center, up_center, down_center,options, msg1, trialType)
 
 % can use the same function with a switch block for all altruism, zili and ziti
 % for altruism and ziti, provide the same which_font which does nothing
@@ -11,14 +11,14 @@ ziti_left = char(item_con(1));
 ziti_right = char(item_con(2));
 
 if which_font == 0
-  % 0=normal,1=bold,2=italic,4=underline,8=outline,32=condense,64=extend.
-  % NOTE: 1 is *BOLD*. Do we want this? If yes, use 1 and 3 instead!
+    % 0=normal,1=bold,2=italic,4=underline,8=outline,32=condense,64=extend.
+    % NOTE: 1 is *BOLD*. Do we want this? If yes, use 1 and 3 instead!
     mark_ziti = 0;
     mark_ziti2 = 2;
 elseif which_font == 1
     mark_ziti = 2;
     mark_ziti2 = 0;
-  elseif isnan(which_font)
+elseif isnan(which_font)
     mark_ziti = 0;
     mark_ziti2 = 0;
 end
@@ -31,7 +31,15 @@ Screen('DrawText',wptr, double(ziti_left),  left_center, down_center, 255);
 Screen(wptr,'TextStyle',mark_ziti2);
 Screen('DrawText',wptr, double(ziti_right), right_center, down_center, 255);
 Screen('Flip', wptr);
-recordEvents(NaN, 31, NaN, NaN);
+
+if trialType == 1
+    recordEvents(NaN, 11, NaN, NaN);
+elseif trialType == 2
+    recordEvents(NaN, 21, NaN, NaN);
+elseif trialType == 3
+    recordEvents(NaN, 31, NaN, NaN);
+end
+
 
 ListenChar(2);
 
@@ -41,12 +49,12 @@ t_now = GetSecs;
 flag_isResponse = 0;
 
 while t_now - t_init < 4
-
+    
     t_now = GetSecs;
-
+    
     if flag_isResponse == 0
         [ keyIsDown, ~, keyCode ] = KbCheck;
-
+        
         % If the user is pressing a key, then display its code number and name.
         if keyIsDown && ~isempty(find(keyCode, 1))
             if keyCode(leftKey)
@@ -58,16 +66,17 @@ while t_now - t_init < 4
                 [~, ~, bboxL] = DrawFormattedText_box(wptr, double(ziti_left), left_center, down_center, WhiteIndex(wptr),  [], [], [], [], 0, bbox_left);
                 Screen(wptr,'TextStyle', mark_ziti2);
                 Screen('DrawText',wptr, double(ziti_right), right_center, down_center, 255);
-
+                
                 rect = bboxL + ziti_size*[-0.3 -0.2 0.3 0.6];
                 Screen('FrameRect', wptr, [255,0,0], rect, 1);
                 Screen('Flip', wptr);
                 flag_isResponse = 1;
-                recordEvents(3, 3, NaN, NaN);
-
+                event_key = 4;
+                %recordEvents(3, 3, NaN, NaN);
+                
             elseif keyCode(rightKey)
                 %bbox_left = [left_center-4*ziti_size down_center left_center+4*ziti_size down_center];
-
+                
                 bbox_right = [right_center-4*ziti_size down_center right_center+4*ziti_size down_center];
                 Screen(wptr,'TextStyle', 0);
                 Screen('DrawText',wptr, double(msg1), xcenter-2*ziti_size, up_center, 255);
@@ -75,30 +84,39 @@ while t_now - t_init < 4
                 Screen('DrawText',wptr, double(ziti_left),  left_center, down_center, 255);
                 Screen(wptr,'TextStyle', mark_ziti2);
                 [~, ~, bboxR] = DrawFormattedText_box(wptr, double(ziti_right), right_center, down_center, WhiteIndex(wptr), [], [], [], [], 0, bbox_right);
-
+                
                 rect = bboxR + ziti_size*[-0.3 -0.2 0.3 0.6];
                 Screen('FrameRect', wptr, [255,0,0], rect, 1);
                 Screen('Flip', wptr);
                 flag_isResponse = 1;
-                recordEvents(4, 3, NaN, NaN);
-
-              elseif keyCode(escapeKey)
+                event_key = 4;
+                %recordEvents(4, 3, NaN, NaN);
+                
+            elseif keyCode(escapeKey)
                 recordEvents(9, 9, 9, 9);
                 error('Manually aborted!')
             end
-
+            
+            if trialType == 1
+                recordEvents(event_key, 12, NaN, NaN);
+            elseif trialType == 2
+                recordEvents(event_key, 22, NaN, NaN);
+            elseif trialType == 3
+                recordEvents(event_key, 32, NaN, NaN);
+            end
+            
             while KbCheck; end %prevent those who HOLDS DOWN the key
         end
     else
         % sub. press a button.
     end
-
+    
     WaitSecs(0.001);
 end
 
 if flag_isResponse == 0
-  % still no response
-  recordEvents(NaN, 3, NaN, NaN); % NOTE: is this code correct?
+    % still no response
+    recordEvents(NaN, 3, NaN, NaN); % NOTE: is this code correct?
 end
 
 ListenChar(0);
