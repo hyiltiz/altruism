6308311083218651f41323926a09a3fe3fa4ab62
Hörmetjan Yiltiz <hyiltiz@gmail.com>
2015-06-06 01:41:56 +0800

    do not add all those data files

--- getSubInfo.m
+++ getSubInfo.m
@@ -2,7 +2,7 @@ function subinfo=getSubInfo()
 prompt = {'被试编号','被试性别[1=男,2=女]','被试年龄','被试利手[1=右利手,2=左利手]'};
 dlg_title = '被试信息';
 num_lines = 1;
-defautanswer = {'01','1','20','1'};
+defautanswer = {'default','1','20','1'};
 subinfo = inputdlg(prompt,dlg_title,num_lines,defautanswer);
 if isempty(subinfo)
     error('Pressed Cancel. Manually aborting!');
